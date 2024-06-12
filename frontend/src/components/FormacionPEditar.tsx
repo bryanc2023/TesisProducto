1dc3bf601e41bc4759cedfa7a8af41d1004ba696
Sebastian Peñaherrera <sebas172010@outlook.es>
2024-06-12 08:17:35 -0500

    cambios perfil

--- frontend/src/components/FormacionPEditar.tsx
+++ frontend/src/components/FormacionPEditar.tsx
@@ -158,7 +158,7 @@ const EditFormacionModal: React.FC<EditFormacionModalProps> = ({ isOpen, closeMo
       isOpen={isOpen}
       onRequestClose={closeModal}
       contentLabel="Editar Formación Académica"
-      className="bg-white p-6 rounded-lg shadow-md w-full max-w-3xl mx-auto my-20 relative"
+      className="bg-white p-4 rounded-lg shadow-md w-full max-w-3xl mx-auto my-20 relative"
       overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
     >
       <button onClick={closeModal} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-2xl font-bold">
