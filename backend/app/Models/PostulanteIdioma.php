2319f1c7814e1abf2b0c08547f43e8bcfe185c75
Sebastian Peñaherrera <sebas172010@outlook.es>
2024-06-16 22:16:24 -0500

    Cambios

--- backend/app/Models/PostulanteIdioma.php
+++ backend/app/Models/PostulanteIdioma.php
@@ -4,33 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class PostulanteIdioma extends Model
 {
     use HasFactory;
+
     protected $table = 'postulante_idioma';
-    protected $primaryKey = ['id_postulante', 'id_idioma'];
     public $incrementing = false;
-    public $timestamps = false; // Desactivar las marcas de tiempo
+    public $timestamps = false;
+    protected $primaryKey = ['id_postulante', 'id_idioma'];
 
     protected $fillable = [
         'id_postulante',
         'id_idioma',
         'nivel_oral',
         'nivel_escrito',
-       
-        // Puedes agregar más campos si es necesario
     ];
 
-    // Relación con el modelo Postulante
     public function postulante()
     {
         return $this->belongsTo(Postulante::class, 'id_postulante');
     }
 
-    // Relación con el modelo Titulo
     public function idioma()
     {
         return $this->belongsTo(Idioma::class, 'id_idioma');
+    }
+
+    protected function setKeysForSaveQuery($query)
+    {
+        return $query->where('id_postulante', $this->getAttribute('id_postulante'))
+                     ->where('id_idioma', $this->getAttribute('id_idioma'));
     }
 }
